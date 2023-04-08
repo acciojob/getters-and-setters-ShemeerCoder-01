ffb287393bb68b382b713c2649403a2d2639858a
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-08 22:09:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,68 @@
 //complete this code
-class Person {
-	constructor(name,age){
-		this.name = name;
-		this.age = age;
-	}
-	getter(){
-		return this.name;
-	}
-	setter(age){
-		this.age = age;
-	}
-}
+// class Person {
+// 	constructor(name,age){
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// 	getter(){
+// 		return this.name;
+// 	}
+// 	setter(age){
+// 		this.age = age;
+// 	}
+// }
 
-class Student extends Person {
+// class Student extends Person {
    
-	study(){
-		console.log(`${this.name} is studying`);
-	}
-}
+// 	study(){
+// 		console.log(`${this.name} is studying`);
+// 	}
+// }
 
-class Teacher extends Person {
+// class Teacher extends Person {
    
     
-	teach(){
-		console.log(`${this.name} is teaching`);
-	}
+// 	teach(){
+// 		console.log(`${this.name} is teaching`);
+// 	}
+// }
+
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  /**
+   * @param {any} age
+   */
+  set age(age) {
+    this._age = age;
+  }
 }
+ 
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+ 
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
 let person1 = new Person("shemeer",24);
 person1.age = 27;
-let student1 = new Student("John",30);
-student1.study();
-let teacher1 = new Teacher("Alice",30);
-teacher1.teach();
+// let student1 = new Student("John",30);
+person1.study();
+// let teacher1 = new Teacher("Alice",30);
+person1.teach();
 
 // Do not change the code below this line
 window.Person = Person;
